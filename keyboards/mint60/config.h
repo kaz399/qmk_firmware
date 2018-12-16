e04fd7d27ff0a6444e78a05c17ef90100043ef15
Kazuhiro.Yabe <Kazuhiro.Yabe@gmail.com>
2018-12-17 05:50:26 +0900

    Add my local layout of Mint60

--- keyboards/mint60/config.h
+++ keyboards/mint60/config.h
@@ -30,7 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DESCRIPTION     A row staggered split keyboard
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 120
 
 /* key matrix size */
 #define MATRIX_ROWS 10
